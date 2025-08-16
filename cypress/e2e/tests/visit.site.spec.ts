0957523bf0ede67f0883f0eb4165a267ec5c04d1
ponfri <phillip.onfri@starz.com>
2025-08-16 16:22:59 -0400

    Deleted file sno longer needed

--- cypress/e2e/tests/visit.site.spec.ts
+++ cypress/e2e/tests/visit.site.spec.ts
@@ -6,12 +6,37 @@
 // Description: Test file for loading website
 
 import { LandingPage } from "../pageObjects/LandingPage.po";
+import { UtilitiesPage } from "../pageObjects/UtilitiesPage.po";
 
 const landingPage = new LandingPage("/");
+const utilitiesPage = new UtilitiesPage();
 
 describe("Template Description here.", () => {
   beforeEach("Run before each test here.", () => {
     landingPage.visit("/"); // BasePage provides visit()
     cy.waitForPageReady();
+  });
+
+  it('[ ] Navigate to Utilities page, check, scroll down/up, go back, and check landing.', { tags: ['@smoke', '@utilities', '@navigation', '@check'] }, () => {
+    // Navigate to Utilities page using the menu
+    landingPage.LinkUtilities.click();
+    cy.waitForPageReady();
+
+    // Check that we are on the correct page
+    utilitiesPage.utilitiesHeader.should('be.visible');
+    cy.url().should('include', '/utilities');
+
+    // Scroll to the end of the page
+    cy.scrollToEnd();
+
+    // Scroll back to the top
+    cy.scrollToTop();
+
+    // Go back to previous page
+    cy.go('back');
+    cy.waitForPageReady();
+    // Check that we are back on the landing page
+    landingPage.Header.should('exist').and('be.visible').and('contain.text', 'Kitchen Sink');
+    cy.url().should('not.include', '/utilities');
   });
 });
