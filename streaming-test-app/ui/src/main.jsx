0f33d0483405d69056a0574900543ed0c4a0ad73
ponfri <phillip.onfri@starz.com>
2025-08-24 15:58:13 -0400

    Added a new project to load a test site locally and also added scripts to main project for launching the site.

---
+++ streaming-test-app/ui/src/main.jsx
@@ -0,0 +1,11 @@
+
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pages/App.jsx'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
